c12e1efa99481a6d205361fea90c5e82f02634d5
john.bryan.j.sazon <john.bryan.j.sazon@accenture.com>
2016-07-15 11:38:01 +0800

    removed the condition to check if network is overlay, its not necessary anymore

--- gitlab-load-platform.sh
+++ gitlab-load-platform.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-# If Infra is Swarm Cluster
-if [[ $(docker network ls | grep $CUSTOM_NETWORK_NAME | grep overlay | wc -l) -gt 0 ]]
-then
-  export DOCKER_HOST=tcp://10.10.2.100:2375
-fi
-
 export INITIAL_WORKSPACE_NAME=${1}
 export INITIAL_PROJECT_NAME={$1}Project
 
